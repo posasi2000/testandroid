a68fdede5d8adfd8579ef85a1b0357fe71822c4c
young <posasi2000@naver.com>
2024-01-17 11:51:02 +0900

    test  11:51

--- day0117/SKUAdapter.java
+++ day0117/SKUAdapter.java
@@ -1,5 +1,6 @@
 package com.example.mytwo;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,8 +15,18 @@ public class SKUAdapter extends BaseAdapter {
     Context mContext = null ;
     LayoutInflater mLayoutInflater = null ;
     ArrayList<SampleData> sample ;
-    Integer[ ]  posterID = { };
-    String[ ] posterName = { };
+    Integer[ ]  posterID = {
+      R.drawable.d11, R.drawable.d22, R.drawable.d33,R.drawable.pizza1,R.drawable.pizza2,R.drawable.pizza3,
+    };
+
+    String[ ] posterName = {" 녹차"," 모래시계"," 나의집"," 초코케익"," 블루케익"," 미니케익", };
+
+        //movieDataList.add(new SampleData(R.drawable.d11, " 녹차", "  주문가능"));
+        //movieDataList.add(new SampleData(R.drawable.d22, " 모래시계", "  주문가능"));
+        //movieDataList.add(new SampleData(R.drawable.d33, " 하우스", "  주문가능"));
+        //movieDataList.add(new SampleData(R.drawable.pizza1, " 초코케익", "  주문가능"));
+        //movieDataList.add(new SampleData(R.drawable.pizza2, " 블루케익", "  주문가능"));
+        //movieDataList.add(new SampleData(R.drawable.pizza3, " 미니케익", "  주문가능"));
 
     public SKUAdapter(Context mContext, ArrayList<SampleData> sample) {
         this.mContext = mContext;
@@ -46,10 +57,32 @@ public class SKUAdapter extends BaseAdapter {
         iv.setImageResource(sample.get(position).getPoster());
         movieName.setText(sample.get(position).getMovieName());
         grade.setText(sample.get(position).getGrade());
+        //여기까지 기술해야 애뮬레이터에 데이터표시
+
+        //2024-01-17-수요일  왼쪽 메인이미지클릭시 상세정보출력
+        final  int pos = position;
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View dialogView = (View)View.inflate(mContext.getApplicationContext(), R.layout.dialog, null);
+                AlertDialog.Builder dlg  = new AlertDialog.Builder(mContext.getApplicationContext());
+                ImageView ivPost = dialogView.findViewById(R.id.ivPoster);
+                TextView  tvPost = dialogView.findViewById(R.id.tvpopup);
+                ivPost.setImageResource(posterID[pos]);
+                tvPost.setText(posterName[pos]);
+                dlg.setTitle(posterName[pos]);
+                dlg.setIcon(R.drawable.x);
+                dlg.setNegativeButton("닫기", null);
+                dlg.show();
+            }
+        });
 
         return view;
     }
 }//class END
+
+
+
 
 
 
